005bae35b39050570d9411b679f1f505ca089584
GlockSwitchMotion <yhmotiondagoat@gmail.com>
2026-08-14 15:39:27 -0600

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -2552,7 +2552,7 @@ function mainapi:CreateGUI()
 	makeDraggable(window)
 	local logo = Instance.new('ImageLabel')
 	logo.Name = 'VapeLogo'
-	logo.Size = UDim2.fromOffset(62, 18)
+	logo.Size = UDim2.fromOffset(160, 18)
     logo.Position = UDim2.fromOffset(11, 10)
 	logo.BackgroundTransparency = 1
 	logo.Image = getcustomasset('mxtionv4/assets/new/guivape.png')
