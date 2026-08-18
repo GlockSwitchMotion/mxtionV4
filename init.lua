a30351795a0fbca7fc8234054ab1d4ae6ddfc3c0
GlockSwitchMotion <yhmotiondagoat@gmail.com>
2026-08-18 16:58:46 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,31 +1,39 @@
+--!nocheck
 local license = ... or {}
-repeat task.wait() until game:IsLoaded()
-if shared.vape then shared.vape:Uninject() end
-license.Key = license.Key or '_key'
+license.Key = script_key or license.Key
 
-local vape
-local loadstring = function(...)
-	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
-	end
-	return res
-end
-local queue_on_teleport = queue_on_teleport or function() end
+local cloneref = cloneref or function(ref) return ref end
 local isfile = isfile or function(file)
 	local suc, res = pcall(function()
 		return readfile(file)
 	end)
 	return suc and res ~= nil and res ~= ''
 end
-local cloneref = cloneref or function(obj)
-	return obj
+local delfile = delfile or function(file)
+	writefile(file, '')
 end
-local playersService = cloneref(game:GetService('Players'))
-local httpService = cloneref(game:GetService("HttpService"))
+
+local downloader = Instance.new('TextLabel')
+downloader.Size = UDim2.new(1, 0, 0, 40)
+downloader.BackgroundTransparency = 1
+downloader.TextStrokeTransparency = 0
+downloader.TextSize = 20
+downloader.TextColor3 = Color3.new(1, 1, 1)
+downloader.Font = Enum.Font.Arial
+downloader.Text = 'Downloading files 0/100'
+downloader.Parent = Instance.new('ScreenGui', gethui and gethui() or cloneref(game:GetService('CoreGui')))
+
+local downloadCount = 0
+local function setDownloadProgress()
+	downloadCount = math.min(downloadCount + 1, 100)
+	downloader.Text = 'Downloading files '..downloadCount..'/100'
+end
 
 local function downloadFile(path, func)
 	if not isfile(path) then
+		if not license.Closet then
+			setDownloadProgress()
+		end
 		local suc, res = pcall(function()
 			return game:HttpGet('https://raw.githubusercontent.com/GlockSwitchMotion/mxtionV4/'..readfile('mxtionv4/profiles/commit.txt')..'/'..select(1, path:gsub('mxtionv4/', '')), true)
 		end)
@@ -36,103 +44,73 @@ local function downloadFile(path, func)
 			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
 		end
 		writefile(path, res)
+		downloader.Text = ''
 	end
 	return (func or readfile)(path)
 end
 
-local function finishLoading()
-	vape.Init = nil
-	vape:Load()
-
-	local teleportedServers
-	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.VapeIndependent) then
-			teleportedServers = true
-			local teleportScript = [[
-				shared.vapereload = true
-				if shared.VapeDeveloper then
-					loadstring(readfile('mxtionv4/main.lua'), 'main')(_scriptconfig)
-				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/GlockSwitchMotion/mxtionV4/'..readfile('mxtionv4/profiles/commit.txt')..'/init.lua', true), 'init')(_scriptconfig)
-				end
-			]]
-			local teleportConfig = httpService:JSONEncode(license)
-			teleportConfig = teleportConfig:gsub('":true', "=true"):gsub('{"', '{')
-			teleportConfig = teleportConfig:gsub(',"', ','):gsub('":', '=')
-			teleportConfig = teleportConfig:gsub('%[', '{'):gsub('%]', '}')
-			teleportScript = teleportScript:gsub('_key', tostring(license.Key or '_key'))
-			teleportScript = teleportScript:gsub('_scriptconfig', teleportConfig)
-			if shared.VapeDeveloper then
-				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
-			end
-			if shared.VapeCustomProfile then
-				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
-			end
-			vape:Save()
-			queue_on_teleport(teleportScript)
+local function wipeFolder(path)
+	if not isfolder(path) then return end
+	for _, file in listfiles(path) do
+		if file:find('init') then continue end
+		if file:find('profile') then continue end
+		if isfile(file) then
+			delfile(file)
+		elseif isfolder(file) then
+			wipeFolder(file)
 		end
-	end))
+	end
+end
 
-	if not shared.vapereload then
-		if getgenv().mxtionrole == 'HWID MISMATCH' then
-			vape:CreateNotification('mxtionV4', 'HWID MISMATCH, Go to the script panel to reset hwid', 25, 'alert')
-			getgenv().mxtionrole = ''
-			task.wait(0.1)
-		end
-		if not shared.vapereload then
-			vape:CreateNotification('mxtionV4', (getgenv().mxtionname and `Authenticated as {getgenv().mxtionname} with {getgenv().mxtionrole}, ` or '').. (vape.VapeButton and 'Press the button in the top right' or 'Press '..table.concat(vape.Keybind, ' + '):upper())..' to open GUI', 5)
-			task.delay(0.05 + cloneref(game:GetService('RunService')).PostSimulation:Wait(), function()
-				if shared.updated then
-					vape:CreateNotification('mxtionV4', `Script has updated from {shared.updated} to {readfile('mxtionv4/profiles/commit.txt')}`, 10, 'info')
-				end
-			end)
-		end	
+
+for _, folder in {'mxtionv4', 'mxtionv4/games', 'mxtionv4/profiles', 'mxtionv4/assets', 'mxtionv4/libraries', 'mxtionv4/guis'} do
+	if not isfolder(folder) then
+		setDownloadProgress()
+		makefolder(folder)
 	end
 end
 
-if not isfile('mxtionv4/profiles/gui.txt') then
-	writefile('mxtionv4/profiles/gui.txt', 'new')
-end
-local gui = 'new'--readfile('mxtionv4/profiles/gui.txt')
-
-if not isfolder('mxtionv4/assets/'..gui) then
-	makefolder('mxtionv4/assets/'..gui)
-end
-vape = loadstring(downloadFile('mxtionv4/guis/'..gui..'.lua'), 'gui')(license)
-shared.vape = vape
-shared.vapesmooth = true
-_G.vape = vape
-getgenv().used_init = true
-
-if hookmetamethod and not getgenv().run then
-	getgenv().run = true
-	local old; old = hookmetamethod(game, '__namecall', function(self, Remote, ...)
-		if not checkcaller() and getnamecallmethod() == 'FireServer' then
-			if typeof(Remote) == "Instance" and Remote.Name == 'TabFreezeAnticheat_ClientToServerReport' then
-				return
-			end
+if not shared.VapeDeveloper then
+	local commit = license.Commit
+	if not commit then
+		local _, subbed = pcall(function() 
+			return game:HttpGet('https://github.com/GlockSwitchMotion/mxtionV4') 
+		end)
+		local commitIdx = subbed and type(subbed) == 'string' and subbed:find('currentOid')
+		commit = commitIdx and subbed:sub(commitIdx + 13, commitIdx + 52) or nil
+		commit = commit and #commit == 40 and commit or nil
+	end
+	
+	commit = commit or (isfile('mxtionv4/profiles/commit.txt') and readfile('mxtionv4/profiles/commit.txt') or 'main')
+	if commit == 'main' or (isfile('mxtionv4/profiles/commit.txt') and readfile('mxtionv4/profiles/commit.txt') or '') ~= commit then
+		if commit ~= 'main' and isfile('mxtionv4/profiles/commit.txt') then
+			shared.updated = readfile('mxtionv4/profiles/commit.txt')
 		end
-		return old(self, Remote, ...)
-	end)
-end
-
-if not shared.VapeIndependent then
-	loadstring(downloadFile('mxtionv4/games/universal.lua'), 'universal')(license)
-	if isfile('mxtionv4/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('mxtionv4/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(license)
-	else
-		if not shared.VapeDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/GlockSwitchMotion/mxtionV4/'..readfile('mxtionv4/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
-			end)
-			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('mxtionv4/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(license)
+		wipeFolder('mxtionv4')
+		wipeFolder('mxtionv4/games')
+		wipeFolder('mxtionv4/guis')
+		wipeFolder('mxtionv4/libraries')
+	end
+	writefile('mxtionv4/profiles/commit.txt', commit)
+	if shared.updated or #listfiles('mxtionv4/profiles') < 4 then
+		shared.VapePresetInstall = function()
+			local suc, req = pcall(request, {
+				Url = 'https://api.github.com/repos/GlockSwitchMotion/mxtionV4/contents/profiles',
+				Method = 'GET'
+			})
+			if not suc or req.StatusCode ~= 200 then return false end
+			local body = cloneref(game:GetService('HttpService')):JSONDecode(req.Body)
+			if not body or typeof(body) ~= 'table' then return false end
+			local installed = false
+			for _, v in body do
+				if v.type == 'file' and pcall(downloadFile, 'mxtionv4/'.. ({v.path:gsub(' ', '%%20')})[1]) then
+					installed = true
+				end
 			end
+			return installed
 		end
 	end
-	loadstring(downloadFile('mxtionv4/libraries/premium.lua'), 'premium')(license)
-	finishLoading()
-else
-	vape.Init = finishLoading
-	return vape
 end
+
+downloader.Text = ''
+return loadstring(downloadFile('mxtionv4/main.lua'), 'main')(license)
