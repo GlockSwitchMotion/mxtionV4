d3efeeb19663b9922807cbf581146c8dfce441a8
GlockSwitchMotion <yhmotiondagoat@gmail.com>
2026-08-23 21:43:43 -0600

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -15523,89 +15523,108 @@ run(function()
 	end)
 end)
 
-
 run(function()
-	local AutoKaliyah
-	local Range
-	local Delay
-	local NoSlow
-	
-	local Legit = getFunctionRange(bedwars.DragonSlayerController.hasEligiblePunchTarget) or 14.4
-	local modifier, old
-	local noSlowUntil = 0
-	
-	local function punch()
-		if NoSlow.Enabled then
-			if not old then
-				modifier = bedwars.SprintController:getMovementStatusModifier()
-				old = modifier.addModifier
-				modifier.addModifier = function(self, tab)
-					if NoSlow.Enabled and tick() < noSlowUntil and tab and tab.moveSpeedMultiplier == 0 then
-						tab.moveSpeedMultiplier = 1
-					end
-					return old(self, tab)
-				end
-	
-				AutoKaliyah:Clean(function()
-					modifier.addModifier = old
-					modifier, old = nil, nil
-					noSlowUntil = 0
-				end)
-			end
-			noSlowUntil = math.max(noSlowUntil, tick() + Delay.Value + 0.1)
-		end
-	
-		task.wait(Delay.Value)
-		bedwars.AbilityController:useAbility('dragon_slayer_punch')
-	end
-	
-	AutoKaliyah = vape.Categories.Kits:CreateModule({
-		Name = 'AutoKaliyah',
-		Function = function(call)
-			if call then
-				repeat
-					if entitylib.isAlive and store.equippedKit == 'dragon_slayer' and bedwars.AbilityController:canUseAbility('dragon_slayer_punch', {disableBlockedAbilityAlert = true}) then
-						local localPosition = entitylib.character.RootPart.Position
-						for target, v in bedwars.DragonSlayerController.dragonEmblems do
-							if v.stackCount >= 1 and target.PrimaryPart and (target.PrimaryPart.Position - localPosition).Magnitude <= Range.Value then
-								punch()
-								break
-							end
-						end
-					end
-					task.wait(0.1)
-				until not AutoKaliyah.Enabled
-			end
-		end,
-		Tooltip = 'Automatically uses the "punch" ability from kaliyah'
-	})
-	NoSlow = AutoKaliyah:CreateToggle({
-		Name = 'No Slow',
-		Default = true,
-		Tooltip = 'Prevents you from being slowed down after using the "Punch" ability'
-	})
-	Range = AutoKaliyah:CreateSlider({
-		Name = 'Range',
-		Min = 1,
-		Max = 20,
-		Default = 18,
-		Suffix = function(val)
-			return val <= 1 and 'stud' or 'studs'
-		end
-	})
-	AutoKaliyah:CreateButton({
-		Name = 'Sync to legit range',
-		Function = function()
-			Range:SetValue(Legit)
-		end
-	})
-	Delay = AutoKaliyah:CreateSlider({
-		Name = 'Delay',
-		Min = 0,
-		Max = 1,
-		Default = 0.1,
-		Decimal = 100
-	})
+    local AutoKaliyah
+    local Range
+    local Delay
+    local NoSlow
+    local StackMode
+    
+    local Legit = getFunctionRange(bedwars.DragonSlayerController.hasEligiblePunchTarget) or 14.4
+    local modifier, old
+    local noSlowUntil = 0
+    
+    local stackValues = {
+        ['S1'] = 1,
+        ['S2'] = 2,
+        ['S3'] = 3
+    }
+    
+    local function punch()
+        if NoSlow.Enabled then
+            if not old then
+                modifier = bedwars.SprintController:getMovementStatusModifier()
+                old = modifier.addModifier
+                modifier.addModifier = function(self, tab)
+                    if NoSlow.Enabled and tick() < noSlowUntil and tab and tab.moveSpeedMultiplier == 0 then
+                        tab.moveSpeedMultiplier = 1
+                    end
+                    return old(self, tab)
+                end
+    
+                AutoKaliyah:Clean(function()
+                    modifier.addModifier = old
+                    modifier, old = nil, nil
+                    noSlowUntil = 0
+                end)
+            end
+            noSlowUntil = math.max(noSlowUntil, tick() + Delay.Value + 0.1)
+        end
+    
+        task.wait(Delay.Value)
+        bedwars.AbilityController:useAbility('dragon_slayer_punch')
+    end
+    
+    AutoKaliyah = vape.Categories.Kits:CreateModule({
+        Name = 'AutoKaliyah',
+        Function = function(call)
+            if call then
+                repeat
+                    if entitylib.isAlive and store.equippedKit == 'dragon_slayer' and bedwars.AbilityController:canUseAbility('dragon_slayer_punch', {disableBlockedAbilityAlert = true}) then
+                        local localPosition = entitylib.character.RootPart.Position
+                        local requiredStacks = stackValues[StackMode.Value] or 1
+                        
+                        for target, v in bedwars.DragonSlayerController.dragonEmblems do
+                            if v.stackCount >= requiredStacks and target.PrimaryPart and (target.PrimaryPart.Position - localPosition).Magnitude <= Range.Value then
+                                punch()
+                                break
+                            end
+                        end
+                    end
+                    task.wait(0.1)
+                until not AutoKaliyah.Enabled
+            end
+        end,
+        Tooltip = 'Automatically uses the "punch" ability from kaliyah'
+    })
+
+    StackMode = AutoKaliyah:CreateDropdown({
+        Name = 'Stack Mode',
+        List = {'S1', 'S2', 'S3'},
+        Default = 'S1',
+        Tooltip = 'Minimum dragon slayer stacks required before activating punch'
+    })
+
+    NoSlow = AutoKaliyah:CreateToggle({
+        Name = 'No Slow',
+        Default = true,
+        Tooltip = 'Prevents you from being slowed down after using the "Punch" ability'
+    })
+
+    Range = AutoKaliyah:CreateSlider({
+        Name = 'Range',
+        Min = 1,
+        Max = 20,
+        Default = 18,
+        Suffix = function(val)
+            return val <= 1 and 'stud' or 'studs'
+        end
+    })
+
+    AutoKaliyah:CreateButton({
+        Name = 'Sync to legit range',
+        Function = function()
+            Range:SetValue(Legit)
+        end
+    })
+
+    Delay = AutoKaliyah:CreateSlider({
+        Name = 'Delay',
+        Min = 0,
+        Max = 1,
+        Default = 0.1,
+        Decimal = 100
+    })
 end)
 
 run(function()
