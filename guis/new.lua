efb80c38dc0d6751fd97c9268ba9e5dc5aa58eb3
GlockSwitchMotion <yhmotiondagoat@gmail.com>
2026-08-25 12:59:51 -0600

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -2562,8 +2562,8 @@ function mainapi:CreateGUI()
 	logo.Parent = window
 	local logov4 = Instance.new('ImageLabel')
 	logov4.Name = 'V4Logo'
-	logov4.Size = UDim2.fromOffset(36, 20)
-	logov4.Position = UDim2.new(0, 124, 0, 10)
+	logov4.Size = UDim2.fromOffset(32, 18)
+	logov4.Position = UDim2.new(0, 124, 0, 11)
 	logov4.BackgroundTransparency = 1
 	logov4.Image = getcustomasset('mxtionv4/assets/new/guiv4.png')
 	logov4.ImageRectOffset = Vector2.new(147, 29)
