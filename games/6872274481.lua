80a43692cebb98d445ec8170c52982490980adef
GlockSwitchMotion <yhmotiondagoat@gmail.com>
2026-08-28 16:53:54 -0600

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -15953,46 +15953,48 @@ run(function()
 end)
 
 run(function()
-	local AutoFreiya
-	local Range
-	local Stacks
-	
-	AutoFreiya = vape.Categories.Kits:CreateModule({
-		Name = 'AutoFreiya',
-		Function = function(callback)
-			if callback then
-				repeat
-					if entitylib.isAlive and store.equippedKit == 'ice_queen' and bedwars.AbilityController:canUseAbility('ice_queen', {disableBlockedAbilityAlert = true}) then
-						local origin = entitylib.character.RootPart.Position
-						for _, v in entitylib.List do
-							if v.Targetable and (v.Character:GetAttribute('IceQueenStacks') or 0) >= Stacks.Value and (v.RootPart.Position - origin).Magnitude <= Range.Value then
-								bedwars.AbilityController:useAbility('ice_queen')
-								break
-							end
-						end
-					end
-					task.wait(0.1)
-				until not AutoFreiya.Enabled
-			end
-		end,
-		Tooltip = 'Automatically detonates ice stacks once enemies are frozen enough'
-	})
-	Range = AutoFreiya:CreateSlider({
-		Name = 'Range',
-		Min = 1,
-		Max = 60,
-		Default = 40,
-		Suffix = function(val)
-			return val <= 1 and 'stud' or 'studs'
-		end
-	})
-	Stacks = AutoFreiya:CreateSlider({
-		Name = 'Stacks',
-		Min = 1,
-		Max = 10,
-		Default = 3,
-		Tooltip = 'Ice stacks an enemy needs before detonating'
-	})
+    local AutoFreiya
+    local Range
+    local Stacks
+    
+    AutoFreiya = vape.Categories.Kits:CreateModule({
+        Name = 'AutoFreiya',
+        Function = function(callback)
+            if callback then
+                repeat
+                    if entitylib.isAlive and store.equippedKit == 'ice_queen' and bedwars.AbilityController:canUseAbility('ice_queen', {disableBlockedAbilityAlert = true}) then
+                        local origin = entitylib.character.RootPart.Position
+                        local requiredStacks = tonumber(Stacks.Value) or 3
+                        for _, v in entitylib.List do
+                            if v.Targetable and (v.Character:GetAttribute('IceQueenStacks') or 0) >= requiredStacks and (v.RootPart.Position - origin).Magnitude <= Range.Value then
+                                bedwars.AbilityController:useAbility('ice_queen')
+                                break
+                            end
+                        end
+                    end
+                    task.wait(0.1)
+                until not AutoFreiya.Enabled
+            end
+        end,
+        Tooltip = 'Automatically detonates ice stacks once enemies are frozen enough'
+    })
+    
+    Range = AutoFreiya:CreateSlider({
+        Name = 'Range',
+        Min = 1,
+        Max = 60,
+        Default = 40,
+        Suffix = function(val)
+            return val <= 1 and 'stud' or 'studs'
+        end
+    })
+    
+    Stacks = AutoFreiya:CreateDropdown({
+        Name = 'Stacks',
+        List = {'1', '2', '3', '4'},
+        Default = '3',
+        Tooltip = 'Ice stacks an enemy needs before detonating'
+    })
 end)
 
 run(function()
